883fe7d180d697ec37ee7acaf71b272377cafaeb
Madhs31 <fernandinha.mf30@gmail.com>
2025-10-22 11:44:15 -0300

    🛠️ feat: Configurando rotas e integração com ProtectedRoute e MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,48 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import Tela_Principal from "./pages/Tela_Principal";
+import Perfil from "./pages/Perfil";
+import MainLayout from "./components/MainLayout";
+import ProtectedRoute from "./components/ProtectedRoute"; 
+
 import "./index.css";
 
 function App() {
-  const [paginaAtual, setPaginaAtual] = useState("home");
   const [isDarkMode, setIsDarkMode] = useState(true);
-
-  const irParaTelaPrincipal = () => {
-    setPaginaAtual("principal");
-  };
-
-  const toggleTheme = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+  const toggleTheme = () => setIsDarkMode(!isDarkMode);
 
   useEffect(() => {
     document.body.setAttribute("data-theme", isDarkMode ? "dark" : "light");
   }, [isDarkMode]);
 
   return (
-    <>
-      {paginaAtual === "home" ? (
-        <Home
-          onNavigate={irParaTelaPrincipal}
-          isDarkMode={isDarkMode}
-          toggleTheme={toggleTheme}
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home isDarkMode={isDarkMode} toggleTheme={toggleTheme} />}
         />
-      ) : (
-        <Tela_Principal isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
-      )}
-    </>
+        <Route
+          path="/login"
+          element={<Login isDarkMode={isDarkMode} toggleTheme={toggleTheme} />}
+        />
+        <Route 
+          path="/signup" 
+          element={<SignUp isDarkMode={isDarkMode} toggleTheme={toggleTheme} />} 
+        />
+
+        <Route element={<ProtectedRoute />}>
+          <Route element={<MainLayout isDarkMode={isDarkMode} toggleTheme={toggleTheme} />}>
+            <Route path="/principal" element={<Tela_Principal />} />
+            <Route path="/perfil" element={<Perfil />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
